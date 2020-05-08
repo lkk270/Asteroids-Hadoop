2e9b1bc2f9abb0f805fcf0be943b7aefc1bc0069
Lee Korff-Korn <leekk270@gmail.com>
2020-05-08 01:14:39 -0400

    simple count of gender column complete

---
+++ Asteroids-Hadoop/src/BaseMapReduce/BaseMapper.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+public class BaseMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
+	private final static IntWritable one = new IntWritable(1);
+	private Text word = new Text();
+	
+	public void map(LongWritable _key, Text value, Context context) throws IOException, InterruptedException {
+		StringTokenizer itr = new StringTokenizer(value.toString().split("\t")[12]);
+		while (itr.hasMoreTokens()) {
+			word.set(itr.nextToken());
+			context.write(new Text(word), one);
+		}
+		
+	}
+}
--- Asteroids-Hadoop/src/dataCleaning/DataCleaingMapper.java
+++ Asteroids-Hadoop/src/dataCleaning/DataCleaingMapper.java
@@ -1,5 +1,21 @@
 package dataCleaning;
 
-public class DataCleaingMapper {
+import java.io.IOException;
 
+import java.util.StringTokenizer;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+public class DataCleaingMapper extends Mapper<LongWritable, Text, Text, Text>{
+	public void map(LongWritable Key, Text value, Context con) throws IOException, InterruptedException{
+		
+	}
+	
 }
